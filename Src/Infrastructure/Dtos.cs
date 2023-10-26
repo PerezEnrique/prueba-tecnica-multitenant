0df85b6eae02ea5194fde26093be879194d12f7f
Jesus Enrique Perez <28enriqueperez@gmail.com>
2023-10-26 12:45:03 -0400

    Fix typo

--- Src/Infrastructure/Dtos.cs
+++ Src/Infrastructure/Dtos.cs
@@ -2,10 +2,14 @@ namespace PruebaTecnicaMultitenant.Src.Infrastructure
 {
     //Dtos de usuarios
     public record CreateUserDto(string Email, string Password, int OrganizationId);
-    public record UserDto(int Id, string Email, OrganiaztionDto Organization);
+    public record UserDto(int Id, string Email, OrganizationDto Organization);
     public record UserCredentialsDto(string Email, string Password);
     
     //Dtos de organizationes
     public record CreateOrganizationDto(string Name, string SlugTenant);
-    public record OrganiaztionDto(int Id, string Name, string SlugTenant);
+    public record OrganizationDto(int Id, string Name, string SlugTenant);
+
+    //Dtos de productos
+    public record CreateProductDto(string Name);
+    public record ProductDto(int Id, string Name);
 }
